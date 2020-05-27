d31557ea36cc3ec4864aca305bb8bc5478ad391e
Karen Flores Yánez <kareen2707@gmail.com>
2020-05-27 18:29:36 +0200

    Prototypes of new read functions

--- User App/Drivers/BSP/Components/se868k3/se868k3.h
+++ User App/Drivers/BSP/Components/se868k3/se868k3.h
@@ -13,7 +13,7 @@
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "SE868K3-reg.h"
+#include <se868k3_reg.h>
 #include <string.h>
 #include "cmsis_os.h" //karen
 
@@ -22,11 +22,10 @@
  * @{
 */
 
-typedef int32_t (*SE868K3_Init_Func)(void); //Karen: parameters are the pointer and size of the RX buffer
+typedef int32_t (*SE868K3_Init_Func)(void);
 typedef int32_t (*SE868K3_DeInit_Func)(void);
 typedef int32_t (*SE868K3_Write_Func)(uint8_t *, uint16_t, uint32_t); //Parameters data, size and timeout
-//typedef int32_t (*SE868K3_Read_Func)(uint8_t *, uint16_t); //Karen: new version
-typedef int32_t (*SE868K3_Read_Func)(uint8_t *, uint16_t, char *);
+typedef int32_t (*SE868K3_Read_Func)(uint8_t *, uint16_t, char*); //Warning this must be implemented using DMA channel. Parameters: data and size
 
 typedef struct
   {
@@ -34,7 +33,6 @@ typedef struct
     SE868K3_DeInit_Func     DeInit;
     SE868K3_Write_Func      Write; // to send commands to the module
     SE868K3_Read_Func       Read; // to read data from the module
-    //SE868K3_GetTick_Func       GetTick;
   } SE868K3_IO_t;
 
   typedef struct // This is equivalent to module_
@@ -57,9 +55,15 @@ typedef struct
  int32_t SE868K3_Init(SE868K3_Object_t *pObj);
  int32_t SE868K3_DeInit(SE868K3_Object_t *pObj);
  int32_t SE868K3_Test(SE868K3_Object_t *pObj);
-// int32_t SE868K3_Read_Packet(SE868K3_Object_t* pObj, uint8_t len);
- int32_t SE868K3_Read_GNRMC_Pck(SE868K3_Object_t* pObj);
-
+ //int32_t SE868K3_Read_Packet(SE868K3_Object_t* pObj, uint8_t len);
+ int32_t SE868K3_Read_RMC(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_GGA(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_GLL(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_GSV(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_RLM(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_VTG(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_ZDA(SE868K3_Object_t *pObj);
+ int32_t SE868K3_Read_GSA(SE868K3_Object_t *pObj);
 
 #ifdef __cplusplus
 }
